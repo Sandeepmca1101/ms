9f8123581cb6f2a36f07e85b8f995bdd00ec21aa
sandeepmca1101 <sandeepmca1101@gmail.com>
2018-10-14 01:27:04 +0530

    APIGAGTEWAYS

---
+++ netflix-zuul-api-gatway-server/src/main/java/com/example/ms/netflixzuulapigatwayserver/NetflixZuulApiGatwayServerApplication.java
@@ -0,0 +1,16 @@
+package com.example.ms.netflixzuulapigatwayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NetflixZuulApiGatwayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatwayServerApplication.class, args);
+	}
+}
