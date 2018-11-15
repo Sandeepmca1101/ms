1d16025035e93d7d8bb00b891b391df742139788
sandeepmca1101 <sandeepmca1101@gmail.com>
2018-11-15 15:13:42 +0530

    RABBITMQ+CLOUDBUS+ZIPKIN

--- currency-conversion-service/src/main/java/com/example/mc/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/mc/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 @RestController
 public class CurrencyConversionController {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());	
 
 	@Autowired
 	private CurrencyExchangeProxy proxy;
@@ -28,9 +31,7 @@ public class CurrencyConversionController {
 		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
 				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,
 				uriVariables);
-
 		CurrencyConversionBean response = responseEntity.getBody();
-
 		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
 				quantity.multiply(response.getConversionMultiple()), response.getPort());
 	}
@@ -38,9 +39,8 @@ public class CurrencyConversionController {
 	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
-
 		CurrencyConversionBean response = proxy.getExchangeValue(from, to);
-
+		logger.info("#######################################{CurrencyConversionController}",response);
 		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
 				quantity.multiply(response.getConversionMultiple()), response.getPort());
 	}
--- currency-exchange-service/src/main/java/com/in28minutes/ms/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/in28minutes/ms/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,7 +2,9 @@ package com.in28minutes.ms.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
@@ -10,5 +12,9 @@ public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
